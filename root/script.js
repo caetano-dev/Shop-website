92491c305ec4b4fed2fa511bc2dd551eab100dfd
Pedro Pires <pcaetanop@gmail.com>
2020-08-31 09:59:53 -0300

    added 1 new item to website

--- root/script.js
+++ root/script.js
@@ -1,7 +1,3 @@
-//Get the div for adding all items
-//const item_place = document.querySelector('.items-place')
-
-//create div for each item's image/name/price
 for (let i = 0; i < 7; i++) {
   const item_slot = document.createElement("div");
   item_slot.setAttribute("class", "item_slot");
@@ -26,3 +22,24 @@ for (let i = 0; i < 7; i++) {
 
   document.getElementById("items-place").appendChild(item_slot);
 }
+const item_slot = document.createElement("div");
+item_slot.setAttribute("class", "item_slot");
+
+const item_image = document.createElement("img");
+item_image.setAttribute("class", "item_image");
+item_image.setAttribute("src", "../images/tshirt.jpg");
+
+const item_name = document.createElement("a");
+item_name.setAttribute("class", "item_name");
+item_name.setAttribute("href", "../buy/buy.html");
+item_name.innerHTML = "Normal T-Shirt";
+
+const item_price = document.createElement("span");
+item_price.setAttribute("class", "item_price");
+item_price.innerHTML = "$100,00";
+
+//append item image/price/name to item_place
+item_slot.appendChild(item_image);
+item_slot.appendChild(item_name);
+item_slot.appendChild(item_price);
+document.getElementById("items-place").appendChild(item_slot);
